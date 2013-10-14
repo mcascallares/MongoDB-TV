802c3cd14a2ca714db581d389189b89182d97665
Matias Cascallares <matiascas@gmail.com>
2013-10-14 12:44:06 +0800

    syntax sugar improvements

--- models/show.js
+++ models/show.js
@@ -16,5 +16,5 @@ showSchema.statics.listNames = function(callback) {
     this.find({}, '_id name', callback);
 };
 
-var Show = mongoose.model('Show', showSchema, 'shows');
-module.exports = Show;
+
+module.exports = mongoose.model('Show', showSchema, 'shows');
