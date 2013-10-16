282c4786e0b3f0ab776621f5d73373111e9e7060
Matias Cascallares <matiascas@gmail.com>
2013-10-16 09:07:00 +0800

    cleanup of serving video code

--- routes/episode.js
+++ routes/episode.js
@@ -28,39 +28,16 @@ exports.show = function(req, res) {
 
 exports.get = function(req, res) {
     var filename = req.params.video;
-    var range = req.headers.range;
-    console.log(range);
     episode.load(filename, function(metadata, stream) {
-        if (!range) {
-            console.log('---200---');
-            res.status(200);
-            res.set({'Content-Type': 'video/mp4'}); // TODO hardcoded?
-            stream.pipe(res);
-        } else {
-            console.log('---206---');
-            var parts = range.replace(/bytes=/, "").split("-");
-            var partialstart = parts[0];
-            var partialend = parts[1];
-
-            var total = metadata.length;
-            var start = parseInt(partialstart, 10);
-            var end = partialend ? parseInt(partialend, 10) : total - 1;
-            var chunksize = (end - start) + 1;
-            console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
-
-            res.status(206);
-            res.set({
-                'Accept-Ranges': 'bytes',
-                'Content-Type': 'video/mp4', // TODO hardcoded
-                'Content-Length': chunksize,
-                'Content-Range': 'bytes ' + start + '-' + end + '/' + total,
-                'Etag': metadata.md5,
-                'Last-Modified': metadata.uploadDate
-            });
-            stream.pipe(res);
-        }
-
-
+        res.status(200);
+        res.set({
+            'Accept-Ranges': 'bytes',
+            'Content-Type': 'video/mp4', // TODO hardcoded
+            'Content-Length': metadata.length,
+            'Etag': metadata.md5,
+            'Last-Modified': metadata.uploadDate
+        });
+        stream.pipe(res);
         // TODO not found? 404?
     });
 };
