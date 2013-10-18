c84f0c75d75cf350bdb7baeeb1c8df9c32b5222f
Matias Cascallares <matiascas@gmail.com>
2013-10-18 15:43:30 +0800

    separate connection for gridfs access, sharing the same connection with mongoose wasn't working fine, deadlock?

--- models/episode.js
+++ models/episode.js
@@ -1,70 +1,81 @@
-var mongoose = require('mongoose'),
+var config = require('../config'),
+    mongoose = require('mongoose'),
     Grid = require('gridfs-stream'),
     mime = require('mime'),
     fs = require('fs');
 
 Grid.mongo = mongoose.mongo;
-var chunkSize = 2 * 1024 * 1024; // 2 megs
+
+var chunkSize = 4 * 1024 * 1024; // 2 megs
 var collection = 'episodes';
 
 
 exports.save = function(path, filename, callback) {
-    var gfs = Grid(mongoose.connection.db);
-    var options = {
-        root: collection,
-        filename: filename,
-        contentType: mime.lookup(path),
-        //chunkSize: chunkSize,
-        metadata: {contentType: mime.lookup(path)} // setting in the root is not working
-    };
-    var writestream = gfs.createWriteStream(options);
-    fs.createReadStream(path).pipe(writestream);
-    writestream.on('error', function(error) {
-        throw new Error('An error occured when saving the file to GridFS');
-    })
-    writestream.on('close', function(file) {
-        console.log('File saved in GridFS with the following metadata');
-        console.log(file);
-        callback(file);
-    })
-
+    var conn = mongoose.createConnection(config.mongo.uri);
+    conn.once('open', function() {
+        var gfs = Grid(conn.db);
+        var options = {
+            root: collection,
+            filename: filename,
+            contentType: mime.lookup(path),
+            chunkSize: chunkSize,
+            metadata: {contentType: mime.lookup(path)} // setting in the root is not working
+        };
+        var writestream = gfs.createWriteStream(options);
+        fs.createReadStream(path).pipe(writestream);
+        writestream.on('error', function(error) {
+            throw new Error('An error occured when saving the file to GridFS');
+        })
+        writestream.on('close', function(file) {
+            console.log('File saved in GridFS with the following metadata');
+            console.log(file);
+            callback(file);
+        })
+    });
 };
 
 
 exports.metadata = function(filename, callback) {
-    var gfs = Grid(mongoose.connection.db);
-    var options = {
-        root: collection,
-        filename: filename
-    };
-    gfs.collection(collection).find({filename:filename}).toArray(function (err, files) {
-        if (err) { callback(err, null); }
+    var conn = mongoose.createConnection(config.mongo.uri);
+    conn.once('open', function() {
+        var gfs = Grid(conn.db);
+        var options = {
+            root: collection,
+            filename: filename
+        };
+        gfs.collection(collection).find({filename:filename}).toArray(function (err, files) {
+            if (err) { callback(err, null); }
 
-        if (files && files.length > 0) {
-            callback(null, files[0]);
-        }
-        callback(null, null);
+            if (files && files.length > 0) {
+                callback(null, files[0]);
+            }
+            callback(null, null);
+        });
     });
 };
 
 
 exports.load = function(filename, callback) {
-    var gfs = Grid(mongoose.connection.db);
-    gfs.collection(collection).find({filename:filename}).toArray(function (err, files) {
-        if (err) {
-            callback(new Error('An error occurred when retrieving video from MongoDB'));
-        }
+    var conn = mongoose.createConnection(config.mongo.uri);
+    conn.once('open', function() {
+        var gfs = Grid(conn.db);
 
-        if (files && files.length > 0) {
-            var metadata = files[0];
-            var options = {
-                filename: filename,
-                root: collection
-            };
-            var readstream = gfs.createReadStream(options);
-            callback(null, metadata, readstream);
-        } else {
-            callback(null, null, null);
-        }
-    })
+        gfs.collection(collection).find({filename:filename}).toArray(function (err, files) {
+            if (err) {
+                callback(new Error('An error occurred when retrieving video from MongoDB'));
+            }
+
+            if (files && files.length > 0) {
+                var metadata = files[0];
+                var options = {
+                    filename: filename,
+                    root: collection
+                };
+                var readstream = gfs.createReadStream(options);
+                callback(null, metadata, readstream);
+            } else {
+                callback(null, null, null);
+            }
+        })
+    });
 };
